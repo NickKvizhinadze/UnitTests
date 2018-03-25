22bad0c74ac9e4d96abe55d6f361ce7f24c8adf7
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2018-03-25 11:00:18 +0400

    Added MathTests

---
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            //Arrange
+            var math = new Math();
+
+            //Act
+            var result = math.Add(1, 2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_WhenFirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            //Arrange
+            var math = new Math();
+            //Act
+            var result = math.Max(2, 1);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+
+        [Test]
+        public void Max_WhenSecondArgumentIsGreater_ReturnTheSecondArgument()
+        {
+            //Arrange
+            var math = new Math();
+
+            //Act
+            var result = math.Max(1, 2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_WhenArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            //Arrange
+            var math = new Math();
+
+            //Act
+            var result = math.Max(2, 2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
