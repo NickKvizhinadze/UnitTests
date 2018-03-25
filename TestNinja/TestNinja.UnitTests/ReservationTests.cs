6093d7318981313eb11086e71b71297437661ef5
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2018-03-25 10:42:13 +0400

    Added TestNinja Project with basic unit tests

---
+++ TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_AdminCancelling_ReturnTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_SameUserCancelling_ReturnTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+            var user = new User();
+            reservation.MadeBy = user;
+
+            //Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            //Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_OtherUserCancelling_ReturnFalse()
+        {
+            //Arrange
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User());
+
+            //Assert
+            Assert.That(result, Is.False);
+        }
+    }
+}
