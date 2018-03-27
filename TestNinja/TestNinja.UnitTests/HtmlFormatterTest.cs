4643af347bc1e68cb00237d16d7866c9fa890eed
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2018-03-28 00:04:23 +0400

    Adding tests for string testing

---
+++ TestNinja/TestNinja.UnitTests/HtmlFormatterTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TextFixture]
+    public class HtmlFormatterTest
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            //Arrange
+            var formatter = new HtmlFormatter();
+
+            //Act
+            var result = formatter.FormatAsBold("abc");
+
+            //Specific Assert
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            //More General
+            //Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            //Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+            //Assert.That(result, Does.Contain("abc"));
+        }
+    }
+}
