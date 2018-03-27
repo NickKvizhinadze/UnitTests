af200dcf7df0259638d3d7414d441fcb33e4cffe
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2018-03-27 23:10:26 +0400

    Refactored MathTests

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -6,56 +6,43 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
+        private Math _math;
+
+        //Setup
+        //TearDown
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
         [Test]
-        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        //[Ignore("Because I wanted to!")]
+        public void Add_WhenCalled_ReturnTheGreaterArgument()
         {
             //Arrange
-            var math = new Math();
 
             //Act
-            var result = math.Add(1, 2);
+            var result = _math.Add(1, 2);
 
             //Assert
             Assert.That(result, Is.EqualTo(3));
         }
 
         [Test]
-        public void Max_WhenFirstArgumentIsGreater_ReturnTheFirstArgument()
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Add_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
         {
             //Arrange
-            var math = new Math();
-            //Act
-            var result = math.Max(2, 1);
-
-            //Assert
-            Assert.That(result, Is.EqualTo(2));
-        }
-
-
-        [Test]
-        public void Max_WhenSecondArgumentIsGreater_ReturnTheSecondArgument()
-        {
-            //Arrange
-            var math = new Math();
 
             //Act
-            var result = math.Max(1, 2);
+            var result = _math.Max(a, b);
 
             //Assert
-            Assert.That(result, Is.EqualTo(2));
-        }
-
-        [Test]
-        public void Max_WhenArgumentsAreEqual_ReturnTheSameArgument()
-        {
-            //Arrange
-            var math = new Math();
-
-            //Act
-            var result = math.Max(2, 2);
-
-            //Assert
-            Assert.That(result, Is.EqualTo(2));
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
