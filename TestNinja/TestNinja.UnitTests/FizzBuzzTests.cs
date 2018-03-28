ecaccf548d476038a3cc7fb77a0112a9a84f7acb
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2018-03-29 01:11:01 +0400

    Added FizzBuzz Tests

---
+++ TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+
+        [Test]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(1, "1")]
+        public void GetOutput_WhenCalled_ReturnCorrectOutput(int number, string result)
+        {
+            var output = FizzBuzz.GetOutput(number);
+
+            Assert.That(output, Is.EqualTo(result));
+        }
+    }
+}
