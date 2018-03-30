cdc0e342a598a2ce8406d221b547e952b64e6299
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2018-03-30 23:50:03 +0400

    Added Stack Tests

---
+++ TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_WhenSpeedIsOutOfRange_ThrowArgumentOutOfRangeException(int speed)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _calculator.CalculateDemeritPoints(speed));
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDetermitPoints_WhenCalled_ReturnDetermitPoints(int speed, int expected)
+        {
+            var result = _calculator.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+    }
+}
---
+++ TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        private Stack<string> _stack;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _stack = new Stack<string>();
+        }
+
+        [Test]
+        public void Count_WhenStackIsEmpty_ReturnZero()
+        {
+            Assert.That(_stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Push_AddNull_ThrowArgumentNullException()
+        {
+            Assert.That(() => _stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_AddObject_IncreaseCount()
+        {
+            _stack.Push("Test");
+
+            Assert.That(_stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Pop_WhenCountIsZero_ThrowInvalidOperationException()
+        {
+            Assert.That(() => _stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_WhenCalled_ReturnObject()
+        {
+            var obj = "Test";
+            _stack.Push(obj);
+
+            var result = _stack.Pop();
+
+            Assert.That(result, Is.EqualTo(obj));
+            Assert.That(_stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Peek_WhenCountIsZero_ThrowInvalidOperationException()
+        {
+            Assert.That(() => _stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_WhenCalled_ReturnObject()
+        {
+            var obj = "Test";
+            _stack.Push(obj);
+
+            var result = _stack.Pop();
+
+            Assert.That(result, Is.EqualTo(obj));
+            Assert.That(_stack.Count, Is.EqualTo(0));
+        }
+    }
+}
